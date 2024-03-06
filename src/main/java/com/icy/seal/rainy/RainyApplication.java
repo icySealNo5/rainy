431db2346b48c336a4dac5c4f0342e18fefd415e
逆风了 <358807496@qq.com>
2024-03-06 17:15:12 +0800

    init

---
+++ src/main/java/com/icy/seal/rainy/RainyApplication.java
@@ -0,0 +1,13 @@
+package com.icy.seal.rainy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RainyApplication.class, args);
+    }
+
+}
